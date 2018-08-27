f41a6ed7bd46c7b080f055f666b2f214b54f304f
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-27 19:14:35 +0530

    namerollnumber

---
+++ m1/Assignment-1/NameRollNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class NameRollNumber
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		int rollnumber = scan.nextInt();
+		String name = scan.nextLine();
+		System.out.println(rollnumber);
+		System.out.println(name);
+	}
+}
