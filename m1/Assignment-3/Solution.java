4c7fbde64be4937fc4930b5ccfe93954876d1bc1
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-27 21:04:09 +0530

    temperature conversion

---
+++ m1/Assignment-3/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double c = scan.nextDouble();
+		double f = 1.8;
+		double celcius = 32;
+		f = f*c;
+		f = f + celcius;
+		System.out.print(f);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
