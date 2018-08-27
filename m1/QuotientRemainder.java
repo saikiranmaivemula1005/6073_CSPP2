ebcfcc71af28f83d838a82b694e182a102fce3b3
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-27 18:29:30 +0530

    quotientremainder

---
+++ m1/QuotientRemainder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class QuotientRemainder
+{
+	public static void main(String[] args)
+	{
+		int a=2,b=1,qu,re;
+		qu = a/b;
+		re = a%b;
+		System.out.println(qu);
+		System.out.println(re);
+	}
+}
