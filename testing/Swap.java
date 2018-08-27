71454adb5bf5811b2178ac3aeca5368a6418d6be
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-27 18:22:45 +0530

    quotientremainder

---
+++ testing/Swap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Swap
+{
+	public static void main(String[] args)
+	{
+		int a = 3,b = 4,temp;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
