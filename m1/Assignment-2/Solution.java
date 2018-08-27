84dd0dd61688d962542c005498720ab0246ca704
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-27 21:06:40 +0530

    area of triangle

---
+++ m1/Assignment-2/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double base = scan.nextDouble();
+		double height = scan.nextDouble();
+		double area;
+		area = (base*height)/2.0;
+		System.out.println(area);
+		
+	}
+	/*
+	Need to fill the areaOfTriangle function and print the output
+	of fahrenheit.
+	*/
+	
+}
