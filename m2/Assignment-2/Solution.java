29ef10c14a7ce04e7279f7c4607dffb5a28008c8
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-28 16:13:56 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -16,8 +16,9 @@ public class Solution {
     */
     public static void rootsOfQuadraticEquation(int a, int b, int c) {
         double r1, r2;
-        r1 = ((-b) + (Math.sqrt((b * b) - 4 * (a * c)))) / (2 * a);
-        r2 = ((-b) - (Math.sqrt((b * b) - 4 * (a * c)))) / (2 * a);
+        int x = 4;
+        r1 = ((-b) + (Math.sqrt((b * b) - x * (a * c)))) / (2 * a);
+        r2 = ((-b) - (Math.sqrt((b * b) - x * (a * c)))) / (2 * a);
         System.out.println(r1 + " " + r2);
     }
 }
