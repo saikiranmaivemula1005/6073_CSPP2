b531d09788e2be6edb45d79a1b752a244ba652b8
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-28 17:07:39 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m2/Assignment-3/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);      
+        double base = s.nextInt();
+        double exponent = s.nextInt();
+        double result=power(base,exponent);
+        System.out.println(result);
+	}
+	/*
+	Need to write the power function and print the output.
+	*/
+	public static int power(double base, double exponent){
+		return (int)Math.pow(base, exponent);
+	}
+}
