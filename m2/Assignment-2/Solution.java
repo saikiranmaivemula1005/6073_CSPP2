0ddbdbdedbc28007f1af041c425d2a12d6e2b6f3
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-28 16:29:59 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 /**
 *Do not modify this main function.
 */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
@@ -22,7 +22,7 @@ public class Solution {
 /**
 *rootsOfQuadraticEquation function.
 */
-    public static void rootsOfQuadraticEquation(int a, int b, int c) {
+    public static void rootsOfQuadraticEquation(final int a,final int b,final int c) {
         double r1, r2;
         int x = 2 + 2;
         r1 = ((-b) + (Math.sqrt((b * b) - x * (a * c)))) / (2 * a);
