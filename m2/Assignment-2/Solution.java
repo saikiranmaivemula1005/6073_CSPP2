b0cc80779d31d0cd3da990ddc881412d852def4a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-28 16:10:31 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -9,18 +9,15 @@ public class Solution {
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		System.out.println(rootsOfQuadraticEquation(a, b, c));
+		rootsOfQuadraticEquation(a, b, c);
 	}
 	/*
 	Need to write the rootsOfQuadraticEquation function and print the output.
 	*/
-	public static int rootsOfQuadraticEquation(int a, int b, int c){
-		int r1, r2; 
-		int[] ar;
-		r1 = ((-b) + ((int)Math.sqrt((b*b) - 4*(a * c))))/ (2 * a);
-		r2 = ((-b) - ((int)Math.sqrt((b*b) - 4*(a * c))))/ (2 * a);
-		// ar[0] = r1;
-		// ar[1] = r2;
-		return r1;
+	public static void rootsOfQuadraticEquation(int a, int b, int c){
+		double r1, r2;
+		r1 = ((-b) + (Math.sqrt((b*b) - 4*(a * c))))/ (2 * a);
+		r2 = ((-b) - (Math.sqrt((b*b) - 4*(a * c))))/ (2 * a);
+		System.out.println( r1+" " +r2);
 	}
 }
