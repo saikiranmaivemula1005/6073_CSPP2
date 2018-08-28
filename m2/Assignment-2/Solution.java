b28808dcd0cf8277ebcadaa718c70cb7d62e29c6
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-28 16:11:52 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -18,6 +18,6 @@ public class Solution {
 		double r1, r2;
 		r1 = (( - b) + (Math.sqrt((b * b) - 4 * (a * c)))) / (2 * a);
 		r2 = (( - b) - (Math.sqrt((b * b) - 4 * (a * c)))) / (2 * a);
-		System.out.println( r1 + " " + r2);
+		System.out.println(r1 + " " + r2);
 	}
 }
