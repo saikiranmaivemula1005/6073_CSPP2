bb1da4ae7e31a34b099ae3c0d3fba9950f018274
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-29 11:07:06 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- Module 3/m3/Assignment-3/Solution.java
+++ Module 3/m3/Assignment-3/Solution.java
@@ -1,8 +1,8 @@
 
 import java.util.Scanner;
-/*
-    Do not modify this main function.
-    */
+/**
+*Do not modify this main function.
+*/
 public class Solution {
     /**
      * { function_description }.
@@ -14,25 +14,24 @@ public class Solution {
      * @param      args  The arguments.
      */
 
-    public static void main(String[] args) {
-
-        Scanner s=new Scanner(System.in);      
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
-     * @param      a     { parameter_description }
-     * @param      b     { parameter_description }
+     * @param      a     { parameter_description }.
+     * @param      b     { parameter_description }.
      *
-     * @return     { description_of_the_return_value }
+     * @return     { description_of_the_return_value }.
      */
-    public static int gcd(int a, int b){
+    public static int gcd(final int a, final int b) {
         int gcd = 1;
-        for(int i = 1; i <= a && i <= b; ++i){
-            if(a % i == 0 && b % i == 0){
+        for(int i = 1; i <= a && i <= b; ++i) {
+            if(a % i == 0 && b % i == 0) {
                     gcd = i;
             }
         }
