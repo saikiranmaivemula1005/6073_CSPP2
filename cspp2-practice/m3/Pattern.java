65ed61cb7e92bfece562b9d2d6d4e05a8d05ebd1
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-29 19:37:42 +0530

    Pattern

---
+++ cspp2-practice/m3/Pattern.java
@@ -0,0 +1,11 @@
+public class Pattern{
+	public static void main(String[] args){
+		int num = 1,i,j;
+		for(i = 1; i <= 5; i++){
+			for (j = 1; j<= i; j++){
+				System.out.print(j);	
+			}
+			System.out.print("\n");
+		}
+	}
+}
