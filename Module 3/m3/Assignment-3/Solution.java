532965834b38663cddc5abd49d9f046f9c7df672
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-29 11:07:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- Module 3/m3/Assignment-3/Solution.java
+++ Module 3/m3/Assignment-3/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
      */
 
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
@@ -30,8 +30,8 @@ public class Solution {
      */
     public static int gcd(final int a, final int b) {
         int gcd = 1;
-        for(int i = 1; i <= a && i <= b; ++i) {
-            if(a % i == 0 && b % i == 0) {
+        for (int i = 1; i <= a && i <= b; ++i) {
+            if (a % i == 0 && b % i == 0) {
                     gcd = i;
             }
         }
