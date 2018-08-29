183ad90cc617cbd51b51a1e07f2e30a16c763faf
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-29 09:21:12 +0530

    convert to while loop

---
+++ Module 3/practice/ConverttoWhileLoop.java
@@ -0,0 +1,10 @@
+public class ConverttoWhileLoop{
+	public static void main(String[] args){
+		int num = 2;
+		while(num <= 10){
+			System.out.println(num);
+			num = num + 2;
+		}
+		System.out.println("GoodBye!");
+	}
+}
