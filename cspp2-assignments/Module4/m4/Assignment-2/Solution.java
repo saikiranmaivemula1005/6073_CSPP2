ca7e039f28655a4f13b571f37191e915a409fd1e
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-30 12:12:21 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module4/m4/Assignment-2/Solution.java
+++ cspp2-assignments/Module4/m4/Assignment-2/Solution.java
@@ -1,28 +1,39 @@
-import java.util.*;
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
+	/**
+	 *Fill the main function to print resultant of addition of matrices.
+	 */
+	protected Solution() { }
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int r1 = s.nextInt();
 		int c1 = s.nextInt();
 		int[][] m1 = new int[r1][c1];
-		for (int i = 0; i < r1; i++){
-			for (int j = 0; j < c1; j++){
+		for (int i = 0; i < r1; i++) {
+			for (int j = 0; j < c1; j++) {
 				m1[i][j] = s.nextInt();
 			}
 		}
 		int r2 = s.nextInt();
 		int c2 = s.nextInt();
 		int[][] m2 = new int[r2][c2];
-		for (int i = 0; i < r2; i++){
-			for (int j = 0; j < c2; j++){
+		for (int i = 0; i < r2; i++) {
+			for (int j = 0; j < c2; j++) {
 				m2[i][j] = s.nextInt();
 			}
 		}
-		if (r1 == r2 && c1 == c2){
+		if (r1 == r2 && c1 == c2) {
 			int[][] m3 = new int[r1][c1];
-			for (int i = 0; i < r1; i++){
-				for (int j = 0; j < c1; j++){
+			for (int i = 0; i < r1; i++) {
+				for (int j = 0; j < c1; j++) {
 					System.out.print(m3[i][j] = m1[i][j] + m2[i][j]);
 					System.out.print(" ");
 			}
