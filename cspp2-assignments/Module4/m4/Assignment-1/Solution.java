8d62bf503e740a804e6c54e12ed0f88854615dcc
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-30 11:01:35 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module4/m4/Assignment-1/Solution.java
+++ cspp2-assignments/Module4/m4/Assignment-1/Solution.java
@@ -9,6 +9,7 @@ public class Solution
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int arr[] = new int[n];
+		System.out.println(n);
 		for (int i = 0; i <= n; i++){
 				arr[i] = sc.nextInt();
 		}
