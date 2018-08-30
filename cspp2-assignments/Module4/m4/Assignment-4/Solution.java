f9baa5f1d48e3c810e7544054d14d39cf9b9c340
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-30 15:42:17 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/Module4/m4/Assignment-4/Solution.java
+++ cspp2-assignments/Module4/m4/Assignment-4/Solution.java
@@ -1,30 +1,38 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-		String reverse=reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	public static String reverseString(String s){
-		int l = s.length();
-		char[] x = s.toCharArray();
-		String reverse = "";
-		// char[] y = new char[l];
-		for(int i = l - 1; i >= 0; i--){
-			// System.out.println(y[i]);
-			reverse = reverse + x[i];
-		}
-			
-	return reverse;
-	}
+/**
+ * Class for solution.
+ */
+public class Solution {
+/**
+    *Do not modify the main function.
+    */
+    protected Solution(){ }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String reverse=reverseString(s);    
+        System.out.println(reverse);
+        
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String reverseString(String s) {
+        int l = s.length();
+        char[] x = s.toCharArray();
+        String reverse = "";
+        for(int i = l - 1; i >= 0; i--) {
+            reverse = reverse + x[i];
+        }
+    return reverse;
+    }
 }
