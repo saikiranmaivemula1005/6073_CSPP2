8d98a34c8c3b59d98db693d1e09fac2d8a3ab1d6
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-30 11:59:45 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module4/m4/Assignment-2/Solution.java
+++ cspp2-assignments/Module4/m4/Assignment-2/Solution.java
@@ -3,11 +3,12 @@ public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		int j;
 		int r1 = s.nextInt();
 		int c1 = s.nextInt();
 		int[][] m1 = new int[r1][c1];
 		for (int i = 0; i < r1; i++){
-			for (int j = 0; j < c1; j++){
+			for (j = 0; j < c1; j++){
 				m1[i][j] = s.nextInt();
 			}
 		}
@@ -15,16 +16,17 @@ public class Solution {
 		int c2 = s.nextInt();
 		int[][] m2 = new int[r2][c2];
 		for (int i = 0; i < r1; i++){
-			for (int j = 0; j < c1; j++){
+			for (j = 0; j < c1; j++){
 				m2[i][j] = s.nextInt();
 			}
 		}
 		if (r1 == r2 && c1 == c2){
 			int[][] m3 = new int[r1][c1];
 			for (int i = 0; i < r1; i++){
-				for (int j = 0; j < c1; j++){
-					System.out.print(m3[i][j] = m1[i][j] + m2[i][j]);
+				for (j = 0; j < c1; j++){
+					m3[i][j] = m1[i][j] + m2[i][j];
 			}
+			System.out.print(m3[i][j]);
 		}
 		}
 		else{
