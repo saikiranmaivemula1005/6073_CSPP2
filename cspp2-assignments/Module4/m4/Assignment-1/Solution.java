9bd5c32c9fb2523ab1877fa88a7e8d19e16b8d8c
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-30 11:31:54 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module4/m4/Assignment-1/Solution.java
+++ cspp2-assignments/Module4/m4/Assignment-1/Solution.java
@@ -6,25 +6,23 @@ import java.util.Arrays;
  */
 public class Solution {
 /**
-	*Fill this main function to print maximum of given array.
-	*/
-	protected Solution() { }
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		// System.out.println(n);
-		for (int i = 0; i < n; i++) {
-			// System.out.println("kk");
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		System.out.println(arr[n - 1]);
-	}
+    *Fill this main function to print maximum of given array.
+    */
+    protected Solution() { }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            // System.out.println("kk");
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[n - 1]);
+    }
 }
