c15c476fc62217b61b80c3411cae196ac960896f
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-01 09:43:17 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- cspp2-assignments/Module 6/m6/Assignment-1/Solution.java
+++ cspp2-assignments/Module 6/m6/Assignment-1/Solution.java
@@ -20,13 +20,15 @@ public final class Solution {
     static void oddComposites(final int n) {
     	for(int i = 0 ; i <= n; i++){
     		if (i % 2 != 0){
-    			System.out.println(i);
-    			if ((i % 1 == 0 && i % i == 0)){
+    			// System.out.println(i);
+    			for(int j = 0; j < n; j++){
+    			if (i % j == 0){
     				System.out.println(i);
     			}
     			else{
     				System.out.println("hello");
     			}
+    		}
     		}
 
     	}
