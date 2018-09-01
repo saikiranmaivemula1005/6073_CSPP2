e52635d85cff54b38dbfb4d894a1081a48d4d931
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-01 11:20:15 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- cspp2-assignments/Module 6/m6/Assignment-2/Solution.java
+++ cspp2-assignments/Module 6/m6/Assignment-2/Solution.java
@@ -29,10 +29,10 @@ final class Solution {
             for (int j = 0; j < columns; j++) {
                 for (int k = 3; k < 11; k = k + 2){
                     int y = 2;
-                    // if (0 < a[i][j] && a[i][j] < x) {
-                    //     a[i][j] = 0;
-                    // }
                     if (0 < a[i][j] && a[i][j] < x) {
+                        a[i][j] = 0;
+                    }
+                    if (x < a[i][j] && a[i][j] < x * k) {
                         a[i][j] = x * y;
                     }
                     y = y + 2;
