18674814e2e6f89623ec16190fff57ee510007cf
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-01 19:10:32 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/Module 4/m4/Assignment-1/Solution.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+public class Solution {
+/**
+    *main function to print maximum of given array.
+    */
+    protected Solution() { }
+    /**
+     *function to print maximum of given array.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            // System.out.println("kk");
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[n - 1]);
+    }
+}
+
