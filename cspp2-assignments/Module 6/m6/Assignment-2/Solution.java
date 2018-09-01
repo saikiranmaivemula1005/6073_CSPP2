d52a9b13d6e5e2270eb32d534d12d85b5db521fb
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-01 10:45:03 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- cspp2-assignments/Module 6/m6/Assignment-2/Solution.java
+++ cspp2-assignments/Module 6/m6/Assignment-2/Solution.java
@@ -26,13 +26,25 @@ final class Solution {
     static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < columns; j++){
-                if (a[i][j] > 50){
-                    a[i][j] = 100;
-                }
-                else{
+                if (0 < a[i][j] && a[i][j]< 50){
                     a[i][j] = 0;
                 }
-
+                if (50 < a[i][j] && a[i][j] < 150){
+                    a[i][j] = 100;
+                }
+                if (151 < a[i][j] && a[i][j] < 250){
+                    a[i][j] = 200;
+                }
+                if (251 < a[i][j] && a[i][j] < 350){
+                    a[i][j] = 300;
+                }
+                if (351 < a[i][j] && a[i][j] < 450){
+                    a[i][j] = 400;
+                }
+                if (451 < a[i][j] && a[i][j] < 550){
+                    a[i][j] = 500;
+                }
+                
                 }
                 
         }
