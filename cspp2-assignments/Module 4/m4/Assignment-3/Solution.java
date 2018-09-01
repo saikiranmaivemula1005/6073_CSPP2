f286749c83e60e67473ddc52d898774621bfe471
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-01 19:16:45 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

---
+++ cspp2-assignments/Module 4/m4/Assignment-3/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * Binary to decimal.
+ */
+import java.util.Scanner;
+/**
+ * class Solution.
+ */
+public final class Solution {
+    /**
+ * class Solution.
+ */
+    protected Solution() { }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+     * function to convert binary to decimal value.
+     *
+     * @param      s  The arguments
+     *@return statement
+     */
+    static String binaryToDecimal(final String s) {
+        int base = 1;
+        int sum = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sum = sum + (base
+             * Character.getNumericValue(s.charAt(i)));
+            base *= 2;
+        }
+
+        return Integer.toString(sum);
+    }
+
+
+}
