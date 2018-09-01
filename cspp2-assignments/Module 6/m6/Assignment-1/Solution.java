61cf26289a7fdcdcdb983ebae2c952cf2c1b945a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-01 09:39:35 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- cspp2-assignments/Module 6/m6/Assignment-1/Solution.java
+++ cspp2-assignments/Module 6/m6/Assignment-1/Solution.java
@@ -21,7 +21,7 @@ public final class Solution {
     	for(int i = 0 ; i <= n; i++){
     		if (i % 2 != 0){
     			System.out.println(i);
-    			if (!(i % 1 == 0 && i % i == 0)){
+    			if ((i % 1 == 0 && i % i == 0)){
     				System.out.println(i);
 
     			}
