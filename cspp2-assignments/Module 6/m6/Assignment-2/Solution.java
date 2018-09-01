20dd31b438182aaf2eda09f8f4272f107ce189cd
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-01 19:24:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/Module 6/m6/Assignment-2/Solution.java
+++ cspp2-assignments/Module 6/m6/Assignment-2/Solution.java
@@ -24,31 +24,10 @@ final class Solution {
      */
     static int[][] roundHundred(final int[][] a,
      final int rows, final int columns) {
-        // final int x = 50, y = 3, z = 5,
-        // k = 7, l = 9, m = 11, p = 4, q = 6, r = 8, s = 10;
+        final float variable = 100f;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                // // for (x = 3; 0 < a[i][i] && a[i][j] < x; )
-                // if (0 < a[i][j] && a[i][j] < x) {
-                //     a[i][j] = 0;
-                // }
-                // if (x < a[i][j] && a[i][j] < x * y) {
-                //     a[i][j] = x * 2;
-                // }
-                // if ((x * y) + 1 <= a[i][j] && a[i][j] < x * z) {
-                //     a[i][j] = x * p;
-                // }
-                // if ((x * z) + 1 < a[i][j] && a[i][j] < x * k) {
-                //     a[i][j] = x * q;
-                // }
-                // if ((x * k) + 1 < a[i][j] && a[i][j] < x * l) {
-                //     a[i][j] = x * r;
-                // }
-                // if ((x * l) + 1 < a[i][j] && a[i][j] < x * m) {
-                //     a[i][j] = x * s;
-                // }
-                // }
-                float x = a[i][j] / 100f;
+                float x = a[i][j] / variable;
                 int y = (int) Math.round(x);
                 a[i][j] = y * 100; 
         }
