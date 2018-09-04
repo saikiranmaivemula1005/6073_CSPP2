0758c6e12a889417b5fcc97886aad35adafceba7
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-04 15:25:55 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- cspp2-assignments/Module 8/m8/CodecampList/List.java
+++ cspp2-assignments/Module 8/m8/CodecampList/List.java
@@ -266,7 +266,7 @@ public class List {
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
                 default:
-                System.out.println("Invalid choice");
+                System.out.print("");
             }
         }
     }
