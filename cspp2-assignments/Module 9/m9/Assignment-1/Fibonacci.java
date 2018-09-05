4532c022d36c5d6297e22ce1007013b4c650ad4d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-05 10:12:22 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/Module 9/m9/Assignment-1/Fibonacci.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+// import List.*;
+
+/*
+ * In the previous module you have created List ADT.
+ * ADTs are developed as reusable libraries
+ * i.e., anyone who needs the ADT can use it in their program
+ * A program that uses the ADT is called a "client" of the ADT
+ * The client typically instantiates an object of the ADT
+ * and invokes the public methods on the ADT object.
+ *
+ * This assignment is to practice the idea of client to the ADT.
+ * Client class is Fibonacci and the client needs to use the List ADT
+ * to store fibonacci numbers in the List ADT.
+ * So, Fibonacci is going to instantiate an object of List ADT
+ * generate fibonacci numbers and add them to the list
+ * by invoking the add method provided in the List ADT.
+ *
+ */
+
+public class Fibonacci extends List{
+    /*
+     * fib is a static method takes a argument n
+     * n is the count of the fibonacci numbers to be generated.
+     * The method returns a List with the n fibonacci numbers.
+     *
+     * Look for the hint.txt if some of the testcases fail.
+     */
+    static List l = new List();
+    public static List fib(int n) {
+        // todo - complete this method
+        l.add(0);
+        l.add(1);
+        int x = 0, y = 1;        
+        for(int i = 1; i < n-1; i++){
+            int fib = x + y;
+            x = y;
+            y = fib;
+            l.add(fib);
+        }
+        return l;
+    }
+
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(fib(n));
+    }
+}
