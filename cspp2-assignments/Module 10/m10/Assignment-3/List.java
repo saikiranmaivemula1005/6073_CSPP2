c8d036a0afaed9d77f31d1e74a1f93dd268b43f8
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-06 13:40:53 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/Module 10/m10/Assignment-3/List.java
+++ cspp2-assignments/Module 10/m10/Assignment-3/List.java
@@ -154,7 +154,11 @@ public class List<E> {
      */
     public boolean contains(E item) {
 		//Write logic for contains method
-        return indexOf(item) == -1;
+        for(int i = 0; i < size; i++) {
+            if(list[i].equals(item))
+                return true;
+        }
+        return false;
     }
     /*
      * Returns the index of the first occurrence 
