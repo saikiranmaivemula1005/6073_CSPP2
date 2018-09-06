b45e1b1c0205be9b62116238025efd1cc321a0e7
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-06 09:57:59 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 10/m10/Assignment-1/List.java
+++ cspp2-assignments/Module 10/m10/Assignment-1/List.java
@@ -287,7 +287,7 @@ public class List {
         if (index < 0){
             System.out.println("Negative Index Exception");
         }
-        for (int i = size; i > index; i--){
+        for (int i = size; i >= index; i--){
             list[i] = list[i - 1];
         }
         list[index] = item;
