4376f8e47695367e57e5db387c30e344169f4e6f
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-06 09:50:41 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 10/m10/Assignment-1/List.java
+++ cspp2-assignments/Module 10/m10/Assignment-1/List.java
@@ -273,7 +273,7 @@ public class List {
         for (int i = size; i < (size + items.length); i++){
             list[i] = items[j];
             j++;
-            // size++;
+            size++;
         }
         // System.out.println(Arrays.toString(list));
     }
