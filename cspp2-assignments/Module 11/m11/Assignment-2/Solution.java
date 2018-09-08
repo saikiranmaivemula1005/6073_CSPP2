260d5b36e9a8581856a1c18e2d61d51fc08d95a0
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-08 17:34:23 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cspp2-assignments/Module 11/m11/Assignment-2/Solution.java
+++ cspp2-assignments/Module 11/m11/Assignment-2/Solution.java
@@ -230,7 +230,8 @@ public class Solution {
                     }
                     String[] arrstring3 = tokens[1].split(",");
                     List object = l.subList(
-                    Integer.parseInt(arrstring3[0]), Integer.parseInt(arrstring3[1]));
+                    Integer.parseInt(arrstring3[0]),
+                    Integer.parseInt(arrstring3[1]));
                     if (object != null) {
                         System.out.println(object);
                     }
