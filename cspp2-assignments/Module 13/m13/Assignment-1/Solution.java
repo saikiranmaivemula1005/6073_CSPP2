05d441d3b6f12bf7295f23af9e2a765c60e36a57
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-08 12:22:06 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module 13/m13/Assignment-1/Solution.java
+++ cspp2-assignments/Module 13/m13/Assignment-1/Solution.java
@@ -82,9 +82,15 @@ class Set {
      
 
     }
-    // public int[][] cartesianProduct(Set setOne, Set setTwo){
-       
-    // }
+    public int[][] cartesianProduct(Set outputSet){
+        int[][] outputArray = new int[5][5];
+        for (int i=0; i<5;i++) {
+            System.out.println(outputSet);
+        }
+
+
+       return outputArray;
+    }
 }
 /**
  * Solution class for code-eval.
@@ -167,15 +173,15 @@ public final class Solution {
                 intArray = intArray(tokens[2]);
                 System.out.println(s.retainAll(intArray));
                 break;
-                // case "cartesianProduct":
-                // s = new Set();
-                // t = new Set();
-                // intArray = intArray(tokens[1]);
-                // s.add(intArray);
-                // intArray = intArray(tokens[2]);
-                // t.add(intArray);
-                // System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
-                // break;
+                case "cartesianProduct":
+                s = new Set();
+                t = new Set();
+                intArray = intArray(tokens[1]);
+                s.add(intArray);
+                intArray = intArray(tokens[2]);
+                t.add(intArray);
+                System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
+                break;
                 default:
                 break;
             }
