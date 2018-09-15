ac6679fb00b438e6ff4984527461f023afd37e8c
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-15 12:38:20 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/Module 19/m19/Assignment-1/Solution.java
+++ cspp2-assignments/Module 19/m19/Assignment-1/Solution.java
@@ -98,7 +98,7 @@ public final class Solution {
         // store the user respones in the quiz object
         String[] newArray = questionArray[1].split(",");
         for (int k = 0; k < questionCounter; k++) {
-        	System.out.println(questionArray[0] +" " + "(" + (k+1) + ")" );
+        	System.out.println("question text" +" " + (k+1) + "(" + (k+1) + ")" );
         	System.out.println(newArray[0] + "	" +newArray[1] +"	"+newArray[2] + "	"+newArray[3]);
         	System.out.println();
         }
