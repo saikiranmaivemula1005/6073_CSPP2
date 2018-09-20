dcf003e7bf861c47aa1ce021489f8ee3444d1328
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-20 17:37:50 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 23/m23/Assignment-1/Project.java
+++ cspp2-assignments/Module 23/m23/Assignment-1/Project.java
@@ -89,7 +89,7 @@ class Project {
 		}
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < length; j++) {
-				System.out.print(matrix[i][j] + " ");
+				System.out.print(matrix[i][j] + "		");
 			}
 			System.out.println();
 		}
