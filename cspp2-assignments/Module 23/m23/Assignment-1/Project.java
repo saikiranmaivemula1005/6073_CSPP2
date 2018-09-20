200c7c6b0ab0f1e461b6b0484f3789440105f197
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-20 16:53:02 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 23/m23/Assignment-1/Project.java
+++ cspp2-assignments/Module 23/m23/Assignment-1/Project.java
@@ -61,7 +61,7 @@ class Document {
         	secondSum += Math.pow(mapTwo.get(inmapTwo),2);
         }
         denominator = Math.sqrt(firstSum) * Math.sqrt(secondSum);
-        double output = (numerator / denominator) * 100;
+        double output = (numerator / denominator);
         return  Math.round(output * 100D) / 100D ;
 	}
 	
