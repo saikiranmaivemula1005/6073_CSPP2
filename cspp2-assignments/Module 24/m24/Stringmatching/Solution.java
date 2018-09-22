1f7d9483875159836e0b83e80b1979fcb47dbd01
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-22 09:51:06 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 24/m24/Stringmatching/Solution.java
+++ cspp2-assignments/Module 24/m24/Stringmatching/Solution.java
@@ -66,5 +66,17 @@ class Solution{
 				matrix[i][j] = s.findLCS(s.toString(filesList[i]),s.toString(filesList[j]));
 			}
 		}
+		System.out.print("      \t");
+		for (int i = 0; i < filesList.length - 1; i++) {
+			System.out.print("\t" + filesList[i].getName());
+		}
+		System.out.println("\t" + filesList[length - 1].getName());
+		for (int i = 0; i < length; i++) {
+			System.out.print(filesList[i].getName() + "\t");
+			for (int j = 0; j < length; j++) {
+				System.out.print(matrix[i][j] + "		");
+			}
+			System.out.println();
+		}
 	}
 }
