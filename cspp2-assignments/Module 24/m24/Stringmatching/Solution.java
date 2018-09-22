42430d7152f5368e03685c0e466f8d4b5170de94
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-22 09:49:43 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ cspp2-assignments/Module 24/m24/Stringmatching/Solution.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.Scanner;
+class Substring {
+	Substring() {
+
+	}
+	public String toString(File givenFile) {
+		String result = "";
+		try {
+			Scanner s = new Scanner(new FileReader(givenFile));
+			StringBuilder sb = new StringBuilder();
+			while (s.hasNext()) {
+				sb.append(s.next());
+				sb.append(" ");
+			}
+			s.close();
+			result = sb.toString();
+		} catch(FileNotFoundException e) {
+			System.out.println("no file");
+		}
+		return result;
+	}
+	public int findLCS(String firstString, String secondString) {
+	int lengthOne = firstString.length();
+	int lengthTwo = secondString.length();
+	double totalLength = lengthOne + lengthTwo;
+	int[][] tempMatrix = new int[lengthOne+1][lengthTwo+1];
+	double result = 0;
+	double lcsValue = 0;
+	for (int i = 0; i <= lengthOne; i++) {
+		for (int j = 0; j <= lengthTwo; j++) {
+			if (i == 0 || j == 0) {
+				tempMatrix[i][j] = 0;
+			} else if (firstString.charAt(i-1) == secondString.charAt(j-1)) {
+				tempMatrix[i][j] = tempMatrix[i-1][j-1]+1;
+			} else {
+				tempMatrix[i][j] = 0;
+			}
+			if (result < tempMatrix[i][j]) {
+				result = tempMatrix[i][j];
+			}
+		}
+	}
+	lcsValue = ((result*2)/totalLength)*100;
+	System.out.println((int)lcsValue);
+	return (int)lcsValue;
+	}
+}
+
+
+class Solution{
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Substring s = new Substring();
+		String path;
+		Scanner scan = new Scanner(System.in);
+		path = scan.nextLine();
+		File folder = new File(path);
+		File[] filesList = folder.listFiles();
+		int length = filesList.length;
+		int[][] matrix = new int[length][length];
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				matrix[i][j] = s.findLCS(s.toString(filesList[i]),s.toString(filesList[j]));
+			}
+		}
+	}
+}
