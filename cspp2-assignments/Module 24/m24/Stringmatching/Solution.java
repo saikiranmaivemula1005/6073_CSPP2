9f9e860e5ce27fc00d86ace7189d5078056f0723
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-22 10:01:26 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 24/m24/Stringmatching/Solution.java
+++ cspp2-assignments/Module 24/m24/Stringmatching/Solution.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.lang.Math;
 class Substring {
 	Substring() {
 
@@ -43,7 +44,7 @@ class Substring {
 	}
 	lcsValue = ((result*2)/totalLength)*100;
 	// System.out.println((int)lcsValue);
-	return lcsValue;
+	return Math.round(lcsValue * 100D) / 100;
 	}
 }
 
