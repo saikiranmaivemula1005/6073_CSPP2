118ba9397c7d8200051bced1c8c2cd8a989ee9e9
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-22 12:01:59 +0530

    "check_style_score -> 12 of 14 passed. style score: 0/1 "

--- cspp2-assignments/Module 25/m25/Assignment-1/Todoist.java
+++ cspp2-assignments/Module 25/m25/Assignment-1/Todoist.java
@@ -18,6 +18,9 @@ public class Todoist {
     			if(listOfTasks.get(i).getImportant().equals("Important") && listOfTasks.get(i).getUrgent().equals("Not Urgent") && listOfTasks.get(i).getStatus().equals("todo")) {
     				System.out.println(listOfTasks.get(i));
     				return;
+    			} else {
+    				System.out.println("null");
+    				return;
     			}
     			 
     		}
