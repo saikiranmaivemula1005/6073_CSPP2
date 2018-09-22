df5e524a80c88c0ff1ea37cc7c6191f48c61d39d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-22 12:01:24 +0530

    "check_style_score -> 12 of 14 passed. style score: 0/1 "

--- cspp2-assignments/Module 25/m25/Assignment-1/Todoist.java
+++ cspp2-assignments/Module 25/m25/Assignment-1/Todoist.java
@@ -15,7 +15,7 @@ public class Todoist {
     public void getNextTask(String name) {
     	for (int i = 0; i < listOfTasks.size(); i++) {
     		if (name.equals(listOfTasks.get(i).getName())) {
-    			if(listOfTasks.get(i).getImportant().equals("Important") && listOfTasks.get(i).getUrgent().equals("Not Urgent")) {
+    			if(listOfTasks.get(i).getImportant().equals("Important") && listOfTasks.get(i).getUrgent().equals("Not Urgent") && listOfTasks.get(i).getStatus().equals("todo")) {
     				System.out.println(listOfTasks.get(i));
     				return;
     			}
