bcc139ff6d91ba4a4ba700890410e58cee01e6b2
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-23 15:54:38 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 24/m24/Bagofwords/Document.java
+++ cspp2-assignments/Module 24/m24/Bagofwords/Document.java
@@ -1,12 +1,3 @@
-import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.io.File;
-import java.io.FileReader;
-import java.util.Map;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
 /**
  * Class for document.
  */
@@ -111,54 +102,5 @@ class Document {
         denominator = Math.sqrt(firstSum) * Math.sqrt(secondSum);
         double output = (numerator / denominator) * hundred;
         return  (int) ((output * hundred) / hundred);
-    }
-}
-/**
- * Class for solution.
- */
-class Solution {
-    /**
-     * constructor fro solution class.
-     */
-    protected Solution() {
-
-    }
-    /**
-     * main method for solution class.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        try {
-        Document d = new Document();
-        String path;
-        Scanner scan = new Scanner(System.in);
-        path = scan.nextLine();
-        File folder = new File(path);
-        File[] list = folder.listFiles();
-        int length = list.length;
-        int[][] matrix = new int[length][length];
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                matrix[i][j] = Document.compare(
-                Document.DocumentToString(list[i]),
-                 Document.DocumentToString(list[j]));
-            }
-        }
-        System.out.print("      \t");
-        for (int i = 0; i < list.length - 1; i++) {
-            System.out.print("\t" + list[i].getName());
-        }
-        System.out.println("\t" + list[length - 1].getName());
-        for (int i = 0; i < length; i++) {
-            System.out.print(list[i].getName() + "\t");
-            for (int j = 0; j < length; j++) {
-    System.out.print(matrix[i][j] + "\t\t");
-            }
-            System.out.println();
-        }
-    } catch (NoSuchElementException e) {
-        System.out.println("empty directory");
-    }
     }
 }
